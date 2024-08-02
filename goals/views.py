a188c38dbd3eda54823ee1081c98f661ba46324b
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-02 21:33:00 +0000

    goals(views.py - create GoalsList class)

--- goals/views.py
+++ goals/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from forge_focus.permissions import OwnerOnly
+from .serializers import GoalsSerializer
+from .models import Goals
+from rest_framework import generics, permissions
 
-# Create your views here.
+
+class GoalsList(generics.ListCreateAPIView):
+    """
+     This view will return a list of goals area for the
+     logged in user, will also provide the user a new
+     area to create new goals.
+    """
+    serializer_class = GoalsSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        """
+        This will add the owner data to the Goals object
+        before it is saved.
+        """
+        serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        """
+        This will pull all the Goals instances that are only linked
+        to the current user. Within this order will be rank first 
+        (with null last), and then created_at.
+        """
+        if self.request.user.is_authenticated:
+            return self.request.user.goals.all().order_by('priority', 'created_at')
+        else:
+            return Goals.objects.none() 
+
+class GoalsDetails(generics.RetrieveUpdateDestroyAPIView):
+    """
+    This view will return a specific goal where 
+    pk will be the ID of the goal.
+    """
+    serializer_class = GoalsSerializer
+    permission_classes = [OwnerOnly]
+    queryset = Goals.objects.all()
