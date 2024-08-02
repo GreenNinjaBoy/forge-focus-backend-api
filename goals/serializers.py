499993dd6a8b38fd648b8f5a139c54338f6ea43d
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-08-02 21:19:15 +0000

    goals(serializers.py - create GoalSerializer)

---
+++ goals/serializers.py
@@ -0,0 +1,50 @@
+from rest_framework import serializers
+from .models import Goals
+
+class GoalsSerializer(serializers.ModelSerializer):
+    """
+    This is the serializer for the goals model. It will change the owner.id into 
+    owner.username. It will add an extra field 'is_owner'. The serializer will 
+    prevent large images from being saved to the database and changes the date
+    fields into an easier format.
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def validate_image(self, value):
+        """
+        This is a custom validation method used 
+        for the image field.
+        This was taken from the Django rest walkthrough 
+        """
+
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Sorry but the Image size is larger than 2mb!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Sorry but the image width is larger than 4096px!'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Sorry but the image height is larger than 4096px!'
+            )
+        return value
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Goals
+        fields = [
+            'id',
+            'owner',
+            'created_at',
+            'updated_at',
+            'name',
+            'priority',
+            'image',
+            'is_owner'
+        ]
