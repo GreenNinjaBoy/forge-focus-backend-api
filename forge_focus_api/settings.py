8bea38911eb154182444b778fb4059e0ca2bc10f
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-09-18 09:25:45 +0000

    settings.py( correct direct API sign in issue)

--- forge_focus_api/settings.py
+++ forge_focus_api/settings.py
@@ -15,13 +15,26 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [(
+if 'DEV' in os.environ:
+    authentication_classes = [
         'rest_framework.authentication.SessionAuthentication'
-        if 'DEV' in os.environ
-        else
+    ]
+else:
+    authentication_classes = [
+        'rest_framework.authentication.SessionAuthentication',
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )],
+    ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': authentication_classes,
+
+    
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [(
+    #     'rest_framework.authentication.SessionAuthentication'
+    #     if 'DEV' in os.environ
+    #     else
+    #     'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    # )],
+    
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DATETIME_FORMAT': '%d %b %y',
