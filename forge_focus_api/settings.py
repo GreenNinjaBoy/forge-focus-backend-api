987c61a13d21faa3f4408b1787de88d2acde319c
J.Connell1990 <68825060+GreenNinjaBoy@users.noreply.github.com>
2024-09-20 19:01:25 +0100

    settings.py( fix refresh issue)

--- forge_focus_api/settings.py
+++ forge_focus_api/settings.py
@@ -27,11 +27,9 @@ else:
         # 'rest_framework.authentication.BasicAuthentication',
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     ]
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': authentication_classes,
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': AUTHENTICATION_CLASSES,
+    'DEFAULT_AUTHENTICATION_CLASSES': authentication_classes,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DATETIME_FORMAT': '%d %b %y',
