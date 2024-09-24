be248773ec88efd86f03c4d9283fa94168551280
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-09-24 19:11:33 +0000

    goals(urls.py - add new path)

--- goals/urls.py
+++ goals/urls.py
@@ -4,4 +4,5 @@ from goals import views
 urlpatterns = [
     path('goals/', views.GoalsList.as_view(), name='goal-list'),
     path('goals/<int:pk>/', views.GoalsDetail.as_view(), name='goal-detail'),
+    path('goals/<int:pk>/delete/', GoalDeleteView.as_view(), name='goal-delete'),
 ]
