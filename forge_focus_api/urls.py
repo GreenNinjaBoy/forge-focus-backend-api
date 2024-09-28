505782f1f5dafc5d092f252c82f0e3b768547657
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-09-28 22:36:21 +0000

    settings.py( attempt API login issue)

--- forge_focus_api/urls.py
+++ forge_focus_api/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import root_route
+from .views import root_route, open_debug_view, logout_route
 
 urlpatterns = [
     path('', root_route),
