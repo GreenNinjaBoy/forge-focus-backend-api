34aec172d74efad2cfbe4172c68876cdcd12ef8e
Jamie Connell <Jamie.Connell_10@hotmail.com>
2024-09-29 21:02:04 +0000

    settings.py(add local host to allowed list)

--- forge_focus_api/settings.py
+++ forge_focus_api/settings.py
@@ -23,6 +23,10 @@ REST_FRAMEWORK = {
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
 
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DATETIME_FORMAT': '%d %b %y',
@@ -54,6 +58,7 @@ ALLOWED_HOSTS = [
     '8000-greenninjab-forgefocusb-pqzs5ywqc6f.ws.codeinstitute-ide.net',
     'forge-focus-api-backend-83543ef108af.herokuapp.com',
     '8000-greenninjab-forgefocusb-iny25r7shzh.ws.codeinstitute-ide.net',
+    'localhost',
 ]
 
 INSTALLED_APPS = [
